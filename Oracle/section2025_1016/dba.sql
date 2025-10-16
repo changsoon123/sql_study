4f3a06f3264f03159b20256a77b81a43190b5f1f
leechangsoon <sool0458@naver.com>
2025-10-16 18:18:01 +0900

    section2025_1016

---
+++ Oracle/section2025_1016/dba.sql
@@ -0,0 +1,9 @@
+SELECT * from hr.employees;
+
+SELECT * from dba_users;
+
+alter user hr identified by hr account unlock;
+
+select * from sys.user$;
+select * from sys.tab$;
+select * from dba_tables;
