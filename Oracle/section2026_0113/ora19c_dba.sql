64054b01b9cafcf4f7768d3caddc2df2a6aed9e1
changsoon123 <sool0458@naver.com>
2026-01-13 17:00:33 +0900

    section2026_0113

---
+++ Oracle/section2026_0113/ora19c_dba.sql
@@ -0,0 +1,11 @@
+select * from v$rman_compression_algorithm;
+
+select * from v$option;
+
+select * from v$rman_encryption_algorithms;
+
+select handle, tag, encrypted from v$backup_piece;
+
+show parameter remote_login_passwordfile;
+
+show parameter sec_case_sensitive_logon;
